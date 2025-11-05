5157e3748f4c40503e7d80fcbabaa54c0ce5e127
ebai03 <ESTEBAN.BAIRES@ucr.ac.cr>
2025-11-04 23:01:21 -0600

    create_backup function
    Makes a new backup using tar at SNAPSHOT_PATH

--- create_snapshot.sh
+++ create_snapshot.sh
@@ -32,6 +32,8 @@ log() {
     shift
     local message="$@"
     local timestamp=$(date '+%Y-%m-%d %H:%M:%S')
+
+    # Append to LOG_FILE and print it on screen
     echo -e "${timestamp} [${level}] ${message}" | tee -a "$LOG_FILE"
 }
 
@@ -45,4 +47,47 @@ check_root () {
     echo "This script must be run as root" 
     exit 1
     fi
+
+    log_success "Verified UID (root)"
+}
+
+# Creates a backup of most system directories, excluding some
+# that were deemed as not necessary to backup.
+# Ref: https://serverfault.com/questions/74696/linux-what-directories-should-i-exclude-when-backing-up-a-server
+create_backup() {
+    log_info "Creating snapshot..."
+
+    mkdir -p "$SNAPSHOT_PATH"
+
+    local exclude_dirs=(
+        '--exclude=/proc/*'
+        '--exclude=/sys/*'
+        '--exclude=/dev/*'
+        '--exclude=/run/*'
+        '--exclude=/tmp/*'
+        '--exclude=/mnt/backups/*'
+        '--exclude=/media/*'
+        '--exclude=/lost+found'
+        '--exclude=/var/log/*'
+        '--exclude=/var/cache/*'
+        '--exclude=/var/tmp/*'
+        '--exclude=.snapshots'
+        '--exclude=$RECYCLE.BIN'
+    )
+
+    log_info "Generating backup, standby..."
+
+    # Creates the backup and logs any errors
+    # ---------------------------------------------
+    # 2>&1 replaces fd 2 (stderr) with fd 1 (stdout)
+    # https://blog.tratif.com/2023/01/09/bash-tips-1-logging-in-shell-scripts/
+    if sudo tar -czf "$SNAPSHOT_PATH/system_backup.tar.gz" \
+        "${exclude_dirs[@]}" \
+        -C / . 2>&1 | grep -v "tar:Removing" | tee -a "$LOG_FILE"; then
+        log_success "Finished backing up"
+    else
+        log_error "Error while generating backup"
+        return 1
+    fi
+
 }
